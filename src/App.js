f534d64a45495dbaa0781963542f50b8a1c6c6ee
BilqisA27 <66884324+BilqisA27@users.noreply.github.com>
2020-09-21 20:50:57 -0700

    Added github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <a
+        href="https://github.com/BilqisA27/my-react-weather-app.git"
+        target="_blank"
+      >
+        {" "}
+        Github link{" "}
+      </a>
+    </div>
+  );
 }
